4f346810fc846592af3ee7c3dd8af1ba47c67763
Gaurav94506 <gauravawasthi94506@gmail.com>
2018-09-19 19:26:15 +0530

    Update Address.java
    some changes on 19-9-2018

--- src/main/java/com/citiustech/contact/model/Address.java
+++ src/main/java/com/citiustech/contact/model/Address.java
@@ -12,6 +12,85 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="Address")
+@EntityListeners(AuditingEntityListener.class)
+@XmlRootElement
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private Long id;
+	@NotNull
+	private String state;
+	public Address()
+	{
+		
+	}
+	public Address(Long id, String state, int pincode) {
+		this.id = id;
+		this.state = state;
+		this.pincode = pincode;
+	}
+	private int pincode;
+	
+	@OneToOne(mappedBy="adr")
+	private Employee employee;
+	@JsonIgnore
+	public Employee getEmp() {
+		return employee;
+	}
+	public void setEmp(Employee emp) {
+		this.employee = emp;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+	
+}
+
+
+/*
+commented on 19-9-2018
+package com.citiustech.contact.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -81,3 +160,4 @@ public class Address {
 	
 	
 }
+*/
