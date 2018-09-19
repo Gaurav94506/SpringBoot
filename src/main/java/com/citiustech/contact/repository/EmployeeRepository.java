f0b8f1c5763e2d017021f1eedf4f5ba7e87992fc
Gaurav94506 <gauravawasthi94506@gmail.com>
2018-09-19 19:27:58 +0530

    Update EmployeeRepository.java
    some changes made on 19

--- src/main/java/com/citiustech/contact/repository/EmployeeRepository.java
+++ src/main/java/com/citiustech/contact/repository/EmployeeRepository.java
@@ -1,5 +1,23 @@
 package com.citiustech.contact.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.citiustech.contact.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+
+	//List<Employee> findByAdrPincode(String name);
+}
+
+/*
+changes made on 19-9-2018
+package com.citiustech.contact.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
