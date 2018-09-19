d329d74e37caa21fb5e5225f55e7ff60f50c8667
Gaurav94506 <gauravawasthi94506@gmail.com>
2018-09-19 19:28:39 +0530

    Update AddressRepository.java
    updated on 19-9-2018

--- src/main/java/com/citiustech/contact/repository/AddressRepository.java
+++ src/main/java/com/citiustech/contact/repository/AddressRepository.java
@@ -1,5 +1,31 @@
 package com.citiustech.contact.repository;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository.*;
+import com.citiustech.contact.model.Address;
+import com.citiustech.contact.model.Employee;
+
+
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+	public Address findByEmployeeId(Long employeeID);
+	
+	/*PagingAndSortingRepository<Employee, Long> repository  // … get access to a bean
+			Page<Employee> users= repository.findAll(new PageRequest(1, 20));*/
+}
+
+/*
+
+package com.citiustech.contact.repository;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,3 +39,5 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
 
 	public List<Address> findByEmployeeId(Long employeeID);
 }
+
+*/
